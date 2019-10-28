1cdae33fd418d6762e9428ed4803a9ca207adacb
BryanHo10 <bryankho1010@gmail.com>
2019-10-28 09:39:04 -0700

    Add AI Class

---
+++ src/Models/Artificial Intelligence/KongAI.js
@@ -0,0 +1,15 @@
+import Player from "../Player"
+
+export default class KongAI extends Player{
+    constructor(label){
+        super(label);
+    } 
+    
+    movePawn(){
+
+    }
+    attackPawn(){
+        
+    }
+
+}
--- src/Models/Player.js
+++ src/Models/Player.js
@@ -19,4 +19,10 @@ export default class Player{
         target.Owner = this.Label;
         this.ActivePawns.push(target);
     }
+    movePawn(){
+
+    }
+    attackPawn(){
+        
+    }
 }
