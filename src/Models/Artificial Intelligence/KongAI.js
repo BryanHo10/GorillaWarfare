e141def2601ebd2a6f2330e51e0a62ca03dd9b3e
BryanHo10 <bryankho1010@gmail.com>
2019-11-01 19:52:53 -0700

    Skeleton of AI

--- src/Models/Artificial Intelligence/KongAI.js
+++ src/Models/Artificial Intelligence/KongAI.js
@@ -3,12 +3,40 @@ import Player from "../Player"
 export default class KongAI extends Player{
     constructor(label){
         super(label);
+        this.heuristicWeights={
+            // Maximize
+            "DmgDealt":1,
+            "DmgKing":1,
+            "PawnKill":1,
+            "KingKill":1,
+            // Minimize
+            "PawnLost":-1,
+            "KingHurt":-1,
+            "KingLost":-1
+        }
+
     } 
-    
-    movePawn(){
+    performAction(grid){
+        
+
 
     }
-    attackPawn(){
+    /**
+     * Returns a heuristic value based on the weights multiplied by delta(P)
+     * @function delta(P): The difference between current Player status and previous Player status  
+     * @param {*} prevStatus 
+     * @param {*} currStatus 
+     */
+    measureWeights(prevStatus,currStatus){
+
+    }
+    calculateDmgDealtHeuristic(){
+        for(let i=0;i<)
+    }
+    movePawn(targetPosition){
+
+    }
+    attackPawn(attackingPawn,targetPawn){
         
     }
 
